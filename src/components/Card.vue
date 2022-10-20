23864417715f0e03896d81a922770b8a2607c7c7
unknown <josearturo.ja912@gmail.com>
2022-10-20 16:12:59 -0500

    primer repositorio

---
+++ src/components/Card.vue
@@ -0,0 +1,44 @@
+<template>
+    <grid>
+        <img :src="url" alt="nombre">
+        <h2> {{ nombre }} </h2>
+        <a v-bind:href="url" target="_blank">Descargar</a>
+    </grid>
+  </template>
+  
+  <style scoped>
+    img {
+        width: 8rem;
+        height: 8rem;
+        padding: 0.5rem;
+        background: snow;
+    }
+    a {
+        text-decoration: none;
+        color: snow;
+        background: #301675;
+        padding: 0.3rem;
+        border-radius: 5px;
+        border: 1px solid #f2f2f2;
+    }
+  </style>
+  
+  <script>
+    import Grid from "./Grid.vue";
+    export default {
+        props: {
+            url: {
+                String,
+            },
+            nombre: {
+                String,
+            },
+  
+        },
+  
+        Components: {
+            Grid,
+        },
+  
+    };
+  </script>
